4a32be0dcf075c4f1edc9f903d1d6e03177a955d
jaimezzapata <zapataval2304@gmail.com>
2025-05-08 20:26:06 -0500

    Introducción al localStorage

---
+++ src/js/login.js
@@ -0,0 +1,62 @@
+let usuarios = [
+  {
+    correo: "correo@correo.com",
+    contrasena: "123456",
+    nombre: "Jaime Zapata",
+    id: 1,
+  },
+  {
+    correo: "admin@correo.com",
+    contrasena: "123456",
+    nombre: "Alejandra Carmona",
+    id: 2,
+  },
+];
+localStorage.setItem("usuarios", JSON.stringify(usuarios));
+
+function buscarUsuario() {
+  let correo = document.getElementById("correo").value;
+  let contrasena = document.querySelector("#contrasena").value;
+  let usuarioEncontrado = usuarios.find(
+    (item) => correo == item.correo && contrasena == item.contrasena
+  );
+  return usuarioEncontrado;
+}
+
+function iniciarSesion() {
+  if (buscarUsuario()) {
+    alertaRedireccion(
+      "Bienvenido",
+      "Será redireccionado en unos segundos",
+      1000,
+      "success"
+    );
+  }
+}
+
+function alertaRedireccion(titulo, mensaje, tiempo, icono) {
+  let timerInterval;
+  Swal.fire({
+    title: titulo,
+    html: mensaje + " <b></b>",
+    timer: tiempo,
+    icon: icono,
+    timerProgressBar: true,
+    didOpen: () => {
+      Swal.showLoading();
+      const timer = Swal.getPopup().querySelector("b");
+      timerInterval = setInterval(() => {
+        timer.textContent = `${Swal.getTimerLeft()}`;
+      }, 100);
+    },
+    willClose: () => {
+      clearInterval(timerInterval);
+      window.location.href = "/src/pages/notes.html";
+    },
+  }).then((result) => {
+    /* Read more about handling dismissals below */
+    if (result.dismiss === Swal.DismissReason.timer) {
+      console.log("I was closed by the timer");
+    }
+  });
+}
